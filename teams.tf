84a188e25968b639682097100df10e7eb93eacfc
James Green <james.green6@digital.justice.gov.uk>
2023-11-27 12:51:31 +0000

    Updated GitHub Permissions
    * Added missing repos to writers group
    * Combined the repos list into one to lower duplication in the codebase.

--- teams.tf
+++ teams.tf
@@ -1,4 +1,45 @@
 locals {
+  team_repositories = [
+    "nvvs-devops",
+    "nvvs-devops-github-actions",
+    "cloud-operations-slack-bot",
+    "mojo-aws-github-oidc-provider",
+    "deployment-tgw",
+    "network-access-control-admin",
+    "network-access-control-disaster-recovery",
+    "network-access-control-infrastructure",
+    "network-access-control-integration-tests",
+    "network-access-control-server",
+    "nvvs-devops-monitor",
+    "PaloAlto-pipelines",
+    "staff-device-dhcp-server",
+    "staff-device-dns-dhcp-admin",
+    "staff-device-dns-dhcp-disaster-recovery",
+    "staff-device-dns-dhcp-infrastructure",
+    "staff-device-dns-server",
+    "staff-device-logging-dns-dhcp-integration-tests",
+    "staff-device-logging-infrastructure",
+    "staff-device-logging-syslog-to-cloudwatch",
+    "staff-device-private-dns-zone",
+    "staff-device-shared-services-infrastructure",
+    "staff-infrastructure-admin-sso",
+    "staff-infrastructure-certificate-services",
+    "staff-infrastructure-metric-aggregation-server",
+    "staff-infrastructure-metric-aggregator-cloud",
+    "staff-infrastructure-monitoring-app-reachability",
+    "staff-infrastructure-monitoring-blackbox-exporter",
+    "staff-infrastructure-monitoring-dns-reachability",
+    "staff-infrastructure-monitoring-snmpexporter",
+    "staff-infrastructure-network-operations",
+    "staff-infrastructure-network-services",
+    "staff-infrastructure-smtp-relay-server",
+    "staff-technology-services-github-teams",
+    "terraform-panorama-config",
+    "transit-gateways",
+    "juniper-mist-integration",
+    "juniper-mist-integration-spike",
+    "tech-docs-monitor"
+  ]
   github_teams = {
     nvvs-devops-admins = {
       description = "To provide administrator access of various repos to the NVVS DevOps team."
@@ -19,47 +60,8 @@ locals {
         "moontune",
         "tommoj"
       ]
-      team_repositories = [
-        "nvvs-devops",
-        "nvvs-devops-github-actions",
-        "cloud-operations-slack-bot",
-        "mojo-aws-github-oidc-provider",
-        "deployment-tgw",
-        "network-access-control-admin",
-        "network-access-control-disaster-recovery",
-        "network-access-control-infrastructure",
-        "network-access-control-integration-tests",
-        "network-access-control-server",
-        "nvvs-devops-monitor",
-        "PaloAlto-pipelines",
-        "staff-device-dhcp-server",
-        "staff-device-dns-dhcp-admin",
-        "staff-device-dns-dhcp-disaster-recovery",
-        "staff-device-dns-dhcp-infrastructure",
-        "staff-device-dns-server",
-        "staff-device-logging-dns-dhcp-integration-tests",
-        "staff-device-logging-infrastructure",
-        "staff-device-logging-syslog-to-cloudwatch",
-        "staff-device-private-dns-zone",
-        "staff-device-shared-services-infrastructure",
-        "staff-infrastructure-admin-sso",
-        "staff-infrastructure-certificate-services",
-        "staff-infrastructure-metric-aggregation-server",
-        "staff-infrastructure-metric-aggregator-cloud",
-        "staff-infrastructure-monitoring-app-reachability",
-        "staff-infrastructure-monitoring-blackbox-exporter",
-        "staff-infrastructure-monitoring-dns-reachability",
-        "staff-infrastructure-monitoring-snmpexporter",
-        "staff-infrastructure-network-operations",
-        "staff-infrastructure-network-services",
-        "staff-infrastructure-smtp-relay-server",
-        "staff-technology-services-github-teams",
-        "terraform-panorama-config",
-        "transit-gateways",
-        "juniper-mist-integration",
-        "juniper-mist-integration-spike"
-      ]
-      permission = "admin"
+      team_repositories = local.team_repositories
+      permission        = "admin"
     }
 
     nvvs-devops-writers = {
@@ -75,46 +77,8 @@ locals {
         "NNavickas1",
         "ukshreya"
       ]
-      team_repositories = [
-        "nvvs-devops",
-        "nvvs-devops-github-actions",
-        "cloud-operations-slack-bot",
-        "mojo-aws-github-oidc-provider",
-        "deployment-tgw",
-        "network-access-control-admin",
-        "network-access-control-disaster-recovery",
-        "network-access-control-infrastructure",
-        "network-access-control-integration-tests",
-        "network-access-control-server",
-        "nvvs-devops-monitor",
-        "PaloAlto-pipelines",
-        "staff-device-dhcp-server",
-        "staff-device-dns-dhcp-admin",
-        "staff-device-dns-dhcp-disaster-recovery",
-        "staff-device-dns-dhcp-infrastructure",
-        "staff-device-dns-server",
-        "staff-device-logging-dns-dhcp-integration-tests",
-        "staff-device-logging-infrastructure",
-        "staff-device-logging-syslog-to-cloudwatch",
-        "staff-device-private-dns-zone",
-        "staff-device-shared-services-infrastructure",
-        "staff-infrastructure-admin-sso",
-        "staff-infrastructure-certificate-services",
-        "staff-infrastructure-metric-aggregation-server",
-        "staff-infrastructure-metric-aggregator-cloud",
-        "staff-infrastructure-monitoring-app-reachability",
-        "staff-infrastructure-monitoring-blackbox-exporter",
-        "staff-infrastructure-monitoring-dns-reachability",
-        "staff-infrastructure-monitoring-snmpexporter",
-        "staff-infrastructure-network-operations",
-        "staff-infrastructure-network-services",
-        "staff-infrastructure-smtp-relay-server",
-        "staff-technology-services-github-teams",
-        "tech-docs-monitor",
-        "terraform-panorama-config",
-        "transit-gateways"
-      ]
-      permission = "push"
+      team_repositories = local.team_repositories
+      permission        = "push"
     }
   }
 }
