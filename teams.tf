96fcec7540f5b49f5c83626e352b643f8b8b7ac2
Stephen James <stephen.james4@digital.justice.gov.uk>
2024-03-05 15:48:27 +0000

    Commenting out tow users to unblock CI
    Suspect that the two MOJ admin staff have not logged in and have been auto removed from the org.

--- teams.tf
+++ teams.tf
@@ -73,10 +73,10 @@ locals {
       ]
       team_members = [
         "tomwells98",
-        "jennycasanova",
+#        "jennycasanova",
         "babatundek",
         "NNavickas1",
-        "ukshreya"
+#        "ukshreya"
       ]
       team_repositories = concat(
         local.common_team_repositories_for_all_groups,
