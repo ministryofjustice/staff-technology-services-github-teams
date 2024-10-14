03bad8082e56d9d21ce078c691411f954a9d03a6
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2024-10-14 11:04:54 +0100

    added aafreen to nvvs-devops-admins group

--- teams.tf
+++ teams.tf
@@ -47,6 +47,7 @@ locals {
     nvvs-devops-admins = {
       description = "To provide administrator access of various repos to the ~NVVS~ DISO DevOps team."
       team_maintainers = [
+        "AafAnsari",       # Aafreen
         "asifamirat00",    # Asif
         "ASTRobinson",     # Aaron
         "Gary-H9",         # Gary
