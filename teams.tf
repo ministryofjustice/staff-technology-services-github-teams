535692c060973c925e013896e87ef87797d9b59a
Sandhya Buddharaju <sandhya.buddharaju@justice.gov.uk>
2025-05-10 07:03:47 +0100

    ND-922 Added Shahid, removed mahmood and added asif back

--- teams.tf
+++ teams.tf
@@ -48,7 +48,7 @@ locals {
       description = "To provide administrator access of various repos to the ~NVVS~ DISO DevOps team."
       team_maintainers = [
         "aaf-moj", # Aafreen
-        #"asifamirat00",    # Asif
+        "asifamirat00",    # Asif
         "ASTRobinson",     # Aaron
         "Gary-H9",         # Gary
         "jamesgreen-moj",  # James
@@ -56,7 +56,7 @@ locals {
         "laurentb4",       # Lauren
         "mitchdawson1982", # Mitch
         "sb21460",         # Sandy
-        "m1moj",           # Mahmood
+        "SMAMoJ",          # Shahid Mahmood
         "shahzad-MOJ",     # Shahzad
         "suri8811",        # Suresh
         "staff-infrastructure-moj",
